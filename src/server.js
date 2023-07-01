a2535a46000fccf44e494ce0dac52115108945b4
T K Saravanan <32513966+tksaravanan@users.noreply.github.com>
2023-07-01 14:05:55 +0530

    Update server.js
    Welcoe=me to DevOps Team

--- src/server.js
+++ src/server.js
@@ -3,8 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Welcome to DevOps CI CD");
-    response.end("....................");
+    response.end("Welcome to DevOps Team");
 
 });
 
